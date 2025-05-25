7d65ad62eb17e8175bda65c0d5d3a6d39d0e4e20
Fellipe Marcel <fmrclms@gmail.com>
2025-05-24 22:08:06 -0300

    feat: update Home component layout for improved structure and styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,11 @@ import Main from "@/components/Main";
 
 export default function Home() {
   return (
-    <>
+    <div className="h-screen flex flex-col">
       <Header />
-      <div className="flex flex-col items-center justify-center min-h-screen p-4 gap-4">
+      <div className="flex flex-col items-center justify-center flex-1">
         <Main />
       </div>
-    </>
+    </div>
   );
 }
