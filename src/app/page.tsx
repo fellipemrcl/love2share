d37813f050a507a768dd0993bfe2c1010d31fdb8
Fellipe Marcel <fmrclms@gmail.com>
2025-05-24 22:02:05 -0300

    feat: add Main component to Home page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import Header from "@/components/Header";
+import Main from "@/components/Main";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <div className="flex flex-col items-center justify-center min-h-screen p-4 gap-4">
+        <Main />
+      </div>
     </>
   );
 }
