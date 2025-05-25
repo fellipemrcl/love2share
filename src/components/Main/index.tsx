104607ee9c9b463829ce1215e003f752790b2e16
Fellipe Marcel <fmrclms@gmail.com>
2025-05-24 22:02:00 -0300

    feat: implement Main component with dialog for creating new groups

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,33 @@
+import { Button } from "../ui/button";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
+const Main = () => {
+  return (
+    <Dialog>
+      <DialogTrigger>
+        <Button className="" variant="default">
+          Novo grupo
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Criar novo grupo</DialogTitle>
+          <DialogDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default Main;
