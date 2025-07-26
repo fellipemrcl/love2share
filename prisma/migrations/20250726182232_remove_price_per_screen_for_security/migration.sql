7d92505d1df2a95c31fc226ee7f1335123f388e4
Fellipe Marcel <fmrclms@gmail.com>
2025-07-26 15:31:47 -0300

    feat: remove pricePerScreen field for security
    - Remove pricePerScreen from database schema for security
    - Prevents direct manipulation of calculated values
    - Price per screen will be calculated dynamically in application
    - Ensures data consistency and prevents tampering

---
+++ prisma/migrations/20250726182232_remove_price_per_screen_for_security/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `pricePerScreen` on the `Streaming` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Streaming" DROP COLUMN "pricePerScreen";
